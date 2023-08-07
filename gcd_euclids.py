43afb43992fb6b3b3fe810666798ba6fa37398c1
Reethu Thota <reethu.thota@gmail.com>
2023-08-07 15:41:27 +0530

    added gcd_euclids.py

---
+++ gcd_euclids.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number 2 : "))
+
+def euclids_algo(num1, num2) :
+    while(num2 != 0) :
+        rem = num1 % num2
+        num1 = num2
+        num2 = rem
+        print("num1 :",num1,"num2 :",num2)
+    return num1
+
+gcd = euclids_algo(num1, num2)
+
+
+print("GCD of {} and {} is {}".format(num1, num2, gcd))
