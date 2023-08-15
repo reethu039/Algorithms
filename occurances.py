42f327abddbcfd0cd6881321a21b9ba91e426164
Reethu Thota <reethu.thota@gmail.com>
2023-08-15 19:16:37 +0530

    Create occurances.py

---
+++ occurances.py
@@ -0,0 +1,21 @@
+#
+def count_occurrences(inputArray):
+    numOccurancesDict = {}
+    
+    for num in inputArray:
+        if num in numOccurancesDict:
+            numOccurancesDict[num] += 1
+        else:
+            numOccurancesDict[num] = 1
+    return numOccurancesDict
+
+sizeOfArray = int(input("Enter size of the array : "))
+inputArray = []
+for i in range(sizeOfArray) :
+    inputArray.append(int(input("Enter number to the array : ")))
+
+result = count_occurrences(inputArray)
+
+print("\nOCCURANCE OF EACH ELEMENT")
+for key, value in result.items():
+    print(f"{key} : {value} occurrences")
