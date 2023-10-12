c695ba14c2ccf4077958af80a818c39502bae616
reethuthota <reethu.thota@gmail.com>
2023-10-12 19:47:18 +0530

    Correct insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,24 +1,32 @@
 def insertionSort(num, arr):
     swap = 0
     comp = 0
-    for i in range(num):
-        key=i
+    for i in range(1,num):
+        temp=arr[i]
         j = i-1
-        while j >= 0 and key < arr[j] :
-            comp = comp + 1
+        comp += 1
+        while j >= 0 and arr[j] > temp:
+            if j==0:
+                comp -= 1
+            comp += 1
             arr[j + 1] = arr[j]
+            swap += 1
             j -= 1
-        arr[j + 1] = key
-    print(swap)
+        arr[j + 1] = temp 
+        
     print(comp)
+    print(swap)
     for i in arr:
         print(i, end=' ')
     
-num = int(input(""))
-arr = list(map(int, input("").split()))
+num = int(input(""))  
 if num==1:
+    arr = list(map(int, input("").split()))
+    print("0")
+    print("0")
     print(arr[0])
 elif num==0:
     print("-1")
 else:
+    arr = list(map(int, input("").split()))
     insertionSort(num, arr)
