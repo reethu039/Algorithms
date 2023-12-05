0ffa7444af0904586a6cddd74d274733133ee6e5
reethuthota <reethu.thota@gmail.com>
2023-12-05 22:40:41 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,20 +1,29 @@
 #Implement bubble sort sorting algorithm to sort the given array of elements.
 
 def bubbleSort(numElements, elements) :
-    for i in range(numElements) :
-        j = i
-        for j in range(numElements-1) :
-            if elements[j] > elements[j+1] :
+    countComparisions = 0
+    countSwapping = 0
+    for i in range(0,numElements-1):  
+        for j in range(numElements-i-1): 
+            countComparisions += 1
+            if(elements[j]>elements[j+1]):  
                 elements[j], elements[j+1] = elements[j+1], elements[j]   
-    print(elements)             
+                countSwapping += 1
+    print(countComparisions,)
+    print(countSwapping,)
+    for k in elements :
+        print(k, end = ' ')
+    
 
 numElements = int(input("Enter no.of elements : "))
-#elements = list(map(int, input("").split()))
 
+#elements = list(map(int, input("").split()))
 elements = []
 for i in range(numElements) :
     ele = int(input("Enter the array elements : "))
     elements.append(ele)
 
-print(elements)
-bubbleSort(numElements, elements)
+if(numElements == 0) :
+    print("-1\n-1\n-1")
+else :
+    bubbleSort(numElements, elements)
