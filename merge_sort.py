f16db039b887725030fdb2a0ae1c105a98717cb9
reethuthota <reethu.thota@gmail.com>
2023-12-06 18:32:56 +0530

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,8 +1,8 @@
-# Python program for implementation of MergeSort
-
 def merge(left, right, arr):
     i = j = k = 0
+    comp = 0
     while (i < len(left)) and (j < len(right)):
+        comp += 1
         if left[i] <= right[j]:
             arr[k] = left[i]
             i += 1
@@ -19,18 +19,30 @@ def merge(left, right, arr):
     while j < len(right):
         arr[k] = right[j]
         j += 1
-        k += 1
+        k += 1 
         
+    return comp
+
 def mergeSort(arr):
+    comp = 0
     if len(arr) > 1:
         mid = len(arr)//2
         left = arr[:mid]
         right = arr[mid:]
-        mergeSort(left)
-        mergeSort(right)
-        merge(left,right,arr)
+        comp_l = mergeSort(left)
+        comp_r= mergeSort(right)
+        comp += comp_l + comp_r
+        c = merge(left, right, arr)
+        comp += c
+        
+    return comp
 
-arr = [12, 11, 13, 5, 6, 7]
-print("Given array is : ",arr)
-mergeSort(arr)
-print("Sorted array is : ", arr)
+size = int(input(""))
+if size != 0:
+    array = list(map(int, input("").split()))
+    comp = mergeSort(array)
+    print(comp)
+    for i in array:
+        print(i, end=' ')
+else:
+    print("-1")
