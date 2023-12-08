9819cec9d5cf57507cd7ef459b2e6ed2f8073196
reethuthota <reethu.thota@gmail.com>
2023-12-08 16:49:03 +0530

    Update heapsort.py

--- SEE/heapsort.py
+++ SEE/heapsort.py
@@ -1,14 +1,14 @@
-def heapify(arr, N, i, swap):
+def heapify(arr, size, i, swap):
     largest = i  # Initialize largest as root
     l = 2 * i + 1     # left = 2*i + 1
     r = 2 * i + 2     # right = 2*i + 2
 
     # See if left child of root exists and is greater than root
-    if l < N and arr[largest] < arr[l]:
+    if l < size and arr[largest] < arr[l]:
         largest = l
 
     # See if right child of root exists and is greater than root
-    if r < N and arr[largest] < arr[r]:
+    if r < size and arr[largest] < arr[r]:
         largest = r
 
     # Change root, if needed
@@ -17,21 +17,21 @@ def heapify(arr, N, i, swap):
         swap[0] += 1  # Increment swap count
 
         # Heapify the root.
-        heapify(arr, N, largest, swap)
+        heapify(arr, size, largest, swap)
 
-def heapSort(arr):
-    N = len(arr)
+def heapSort(array):
+    size = len(array)
     swap = [0]  # Store swap count in a list to maintain reference
 
     # Build a maxheap.
-    for i in range(N//2 - 1, -1, -1):
-        heapify(arr, N, i, swap)
+    for i in range(size//2 - 1, -1, -1):
+        heapify(array, size, i, swap)
 
     # One by one extract elements
-    for i in range(N-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
+    for i in range(size-1, 0, -1):
+        array[i], array[0] = array[0], array[i]  # swap
         swap[0] += 1  # Increment swap count
-        heapify(arr, i, 0, swap)
+        heapify(array, i, 0, swap)
 
     return swap[0]  # Return the total swap count
 
