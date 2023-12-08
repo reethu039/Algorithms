c943bf3a9c7637b6307e626d42296b9c9066a1e6
reethuthota <reethu.thota@gmail.com>
2023-12-08 18:34:25 +0530

    Update prims.py

--- SEE/prims.py
+++ SEE/prims.py
@@ -1,38 +1,33 @@
-INF = 9999
-
-def prims(G, vertices):
-    selected = [False] * MAX
-
+def prims(graph, vertices):
+    selected = [False] * vertices
     selected[0] = True
-    edge_count = 0
-    min_cost = 0
-
+    edge_count = min_cost = 0
+    
     while edge_count < vertices - 1:
         min = INF
-        y = 0
-
+        s = 0
         for i in range(vertices):
-            if selected[i]:
+            if selected[i]==True:
                 for j in range(vertices):
-                    if not selected[j] and G[i][j] < min:
-                        min = G[i][j]
-                        y = j
-
+                    if (selected[j]==False) and (graph[i][j] < min):
+                        min = graph[i][j]
+                        s = j
         min_cost += min
-        selected[y] = True
+        selected[s] = True
         edge_count += 1
-
+        
     print(min_cost)
-
+    
 
 MAX = 100
-G = [[INF] * MAX for i in range(MAX)]
+INF = 9999
+Graph = [([INF] * MAX) for i in range(MAX)]
 
 vertices = int(input(""))
 edges = int(input(""))
 
 for i in range(edges):
-    u, v, w = map(int, input().split())
-    G[u - 1][v - 1] = w
+    u, v, w = map(int, input("").split())
+    Graph[u-1][v-1] = w
 
-prims(G, vertices)
+prims(Graph, vertices)
