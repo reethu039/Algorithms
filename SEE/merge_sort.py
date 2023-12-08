2319a325c16c5c04e4a897d1296e9d33991bd21f
reethuthota <reethu.thota@gmail.com>
2023-12-08 16:36:13 +0530

    Update merge_sort.py

--- SEE/merge_sort.py
+++ SEE/merge_sort.py
@@ -1,44 +1,43 @@
-def merge(left, right, arr):
+def merge(left, right, array):
     i = j = k = 0
     comp = 0
     while (i < len(left)) and (j < len(right)):
-        comp += 1
-        if left[i] <= right[j]:
-            arr[k] = left[i]
-            i += 1
+        comp+=1
+        if left[i] < right[j]:
+            array[k] = left[i]
+            i+=1
         else:
-            arr[k] = right[j]
-            j += 1
-        k += 1
+            array[k] = right[j]
+            j+=1
+        k+=1
     
-    while i < len(left):
-        arr[k] = left[i]
-        i += 1
-        k += 1
-    
-    while j < len(right):
-        arr[k] = right[j]
-        j += 1
-        k += 1 
+    while (i < len(left)):
+        array[k] = left[i]
+        i+=1
+        k+=1
         
-    return comp
+    while (j < len(right)):
+        array[k] = right[j]
+        j+=1
+        k+=1
+    
+    return comp           
 
-def mergeSort(arr):
+def mergeSort(array):
     comp = 0
-    if len(arr) > 1:
-        mid = len(arr)//2
-        left = arr[:mid]
-        right = arr[mid:]
-        comp_l = mergeSort(left)
-        comp_r= mergeSort(right)
-        comp += comp_l + comp_r
-        c = merge(left, right, arr)
-        comp += c
-        
+    if len(array) > 1:
+        mid = len(array)//2
+        left = array[:mid]
+        right = array[mid:]
+        c_left = mergeSort(left)
+        c_right = mergeSort(right)
+        c = merge(left, right, array)
+        comp += c + c_left + c_right
+    
     return comp
 
 size = int(input(""))
-if size != 0:
+if size!=0:
     array = list(map(int, input("").split()))
     comp = mergeSort(array)
     print(comp)
