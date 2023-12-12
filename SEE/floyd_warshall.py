a784496598e3f9596a3512c72dea5076e2d09592
reethuthota <reethu.thota@gmail.com>
2023-12-12 21:21:16 +0530

    Update floyd_warshall.py

--- SEE/floyd_warshall.py
+++ SEE/floyd_warshall.py
@@ -1,26 +1,24 @@
 def floyd_warshall(graph, vertices):
-
     for k in range(vertices):
         for i in range(vertices):
             for j in range(vertices):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
+                graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+    
     for i in range(vertices):
         for j in range(vertices):
-            if i != j:
-                print(f"{i + 1} {j + 1} {graph[i][j]}")
-
+            if i!=j:
+                print(i+1, j+1, graph[i][j])
 
 vertices = int(input(""))
 edges = int(input(""))
+INF = 9999
 
-if edges != 0:
-    graph = [([9999] * vertices) for i in range(vertices)]
-    
+if edges!=0:
+    graph = [([INF] * vertices) for i in range(vertices)]
     for i in range(edges):
-        source, dest, weight = map(int, input().split())
-        graph[source - 1][dest - 1] = weight
+        u, v, weight = map(int, input("").split())
+        graph[u-1][v-1] = weight
     floyd_warshall(graph, vertices)
     
 else:
-    print(-1)
+    print("-1")
