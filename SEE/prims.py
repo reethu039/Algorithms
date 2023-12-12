ac2c0bea65063c30dabb42d300ffc705575d56c6
reethuthota <reethu.thota@gmail.com>
2023-12-12 21:21:19 +0530

    Update prims.py

--- SEE/prims.py
+++ SEE/prims.py
@@ -3,31 +3,29 @@ def prims(graph, vertices):
     selected[0] = True
     edge_count = min_cost = 0
     
-    while edge_count < vertices - 1:
+    while edge_count < vertices-1:
         min = INF
         s = 0
         for i in range(vertices):
             if selected[i]==True:
                 for j in range(vertices):
-                    if (selected[j]==False) and (graph[i][j] < min):
+                    if selected[j]==False and graph[i][j]<min:
                         min = graph[i][j]
                         s = j
         min_cost += min
         selected[s] = True
         edge_count += 1
-        
+    
     print(min_cost)
     
 
-MAX = 100
-INF = 9999
-Graph = [([INF] * MAX) for i in range(MAX)]
-
 vertices = int(input(""))
 edges = int(input(""))
+INF = 9999
+graph = [([INF] * vertices) for i in range(vertices)]
 
 for i in range(edges):
-    u, v, w = map(int, input("").split())
-    Graph[u-1][v-1] = w
+    u, v, weight = map(int, input("").split())
+    graph[u-1][v-1] = weight
 
-prims(Graph, vertices)
+prims(graph, vertices)
