4c3c6a177d6776934cb79e7595203bac0a32acdc
reethuthota <reethu.thota@gmail.com>
2023-12-13 08:18:07 +0530

    Update dijkstra.py

--- SEE/dijkstra.py
+++ SEE/dijkstra.py
@@ -1,42 +1,41 @@
-def dijkstra(Graph, vertices):
+def dijkstra(graph, vertices):
     start = 0
-    distance = [Graph[start][i] for i in range(vertices)]
-    pred = [start] * vertices
+    distance = [graph[start][i] for i in range(vertices)]
     visited = [False] * vertices
-
-    distance[start] = 0
     visited[start] = True
-
-    for count in range(vertices - 1):
+    
+    for count in range(vertices-1):
         min_distance = INF
         next_node = 0
-
+        
         for i in range(vertices):
             if distance[i] < min_distance and not visited[i]:
                 min_distance = distance[i]
                 next_node = i
-
         visited[next_node] = True
-        for i in range(vertices):
-            if not visited[i]:
-                if min_distance + Graph[next_node][i] < distance[i]:
-                    distance[i] = min_distance + Graph[next_node][i]
-                    pred[i] = next_node
-
-    # Printing the shortest paths
+        
+        for j in range(vertices):
+            if not visited[j]:
+                if min_distance + graph[next_node][j] < distance[j]:
+                    distance[j] = min_distance + graph[next_node][j]
+     
     for i in range(vertices):
-        if i != start:
-            print(start + 1, i + 1, distance[i])
+        if i!=start:
+            print(start+1, i+1, distance[i])
+    
 
-MAX = 10
+vertices = int(input(""))
+edges = int(input(""))
 INF = 9999
-Graph = [([INF] * MAX) for i in range(MAX)]
 
-vertices = int(input())
-edges = int(input())
-
-for i in range(edges):
-    u, v, w = map(int, input().split())
-    Graph[u - 1][v - 1] = w
-
-dijkstra(Graph, vertices)
+if vertices!=0 and edges!=0:
+    graph = [([INF] * vertices) for i in range(vertices)]
+    
+    for i in range(vertices):
+        graph[i][i] = 0
+    
+    for i in range(edges):
+        u, v, weight = map(int, input("").split())
+        graph[u-1][v-1] = weight
+    
+    dijkstra(graph, vertices)
